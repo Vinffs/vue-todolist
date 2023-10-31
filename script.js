fddf8cae3db5b46485968038b53d130e81b1027e
Vinffs <vin.demiranda@gmail.com>
2023-10-31 15:51:30 +0100

    generated dinamically todolist

---
+++ script.js
@@ -0,0 +1,31 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          text: "Do the dishes",
+          done: false,
+        },
+        {
+          id: 2,
+          text: "Do laundry",
+          done: true,
+        },
+        {
+          id: 3,
+          text: "Take the trash out",
+          done: false,
+        },
+        {
+          id: 4,
+          text: "Vacuum the living room",
+          done: true,
+        },
+      ],
+      index: 0,
+    };
+  },
+}).mount("#app");
